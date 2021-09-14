e07b827233c8b42e2378b95ec58df7f2b3a6c8f0
akhil <88570370+akhil-repo-git@users.noreply.github.com>
2021-09-14 17:49:08 +0530

    express first assignment
    this is dampel desc

---
+++ express/server.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const   app = express();
+const data = require('./data.json');
+app.listen(5000,()=>{
+    console.log("Listening on port " + 5000)
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello from express");
+})
+
+app.get('/users',(req,res)=>{
+
+
+
+    res.send({users:data});
+
+
+});
